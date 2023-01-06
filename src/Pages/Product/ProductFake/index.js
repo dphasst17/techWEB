0256c651ee8b2c29dcc7c4eb312797334675462d
dphasst17 <phathuu12@gmail.com>
2023-01-06 10:28:57 +0700

    edit

--- src/Pages/Product/ProductFake/index.js
+++ src/Pages/Product/ProductFake/index.js
@@ -4,7 +4,7 @@ const product = [
             "image":"https://asset.msi.com/resize/image/global/product/product_1642749989ec287c623da4e370e98a225c7e137c42.png62405b38c58fe0f07fcef2367d8a9ba1/400.png",
             "title":"MSI GS66 Stealth",
             "price":1500,
-            "count": 1,
+            "quantity": 1,
             "total":0
             
             
@@ -15,7 +15,7 @@ const product = [
             "image":"https://asset.msi.com/resize/image/global/product/product_1650005024261a1da4b5a02c3a5d2e53f259c2fab5.png62405b38c58fe0f07fcef2367d8a9ba1/400.png",
             "title":"MSI Stealth GS77 - 12U",
             "price":1700,
-            "count": 1,
+            "quantity": 1,
             "total":0
             
             
@@ -26,7 +26,7 @@ const product = [
             "image":"https://asset.msi.com/resize/image/global/product/product_16403263621d06828860ebf54360a2ad7bdcf38a17.png62405b38c58fe0f07fcef2367d8a9ba1/400.png",
             "title":"MSI Stealth GS66 - 12U",
             "price":1630,
-            "count": 1,
+            "quantity": 1,
             "total":0
             
             
@@ -37,7 +37,7 @@ const product = [
             "image":"https://asset.msi.com/resize/image/global/product/product_164274948600bd9829a1c621e28437bdd2973ec77c.png62405b38c58fe0f07fcef2367d8a9ba1/400.png",
             "title":"MSI Stealth 15M",
             "price":1500,
-            "count": 1,
+            "quantity": 1,
             "total":0
             
             
@@ -48,7 +48,7 @@ const product = [
             "image":"https://asset.msi.com/resize/image/global/product/product_16480190044810e4802b5bfaf2120e088073690ec5.png62405b38c58fe0f07fcef2367d8a9ba1/400.png",
             "title":"MSI GS66 Stealth",
             "price":1510,
-            "count": 1,
+            "quantity": 1,
             "total":0
             
             
@@ -60,7 +60,7 @@ const product = [
             "image":"https://asset.msi.com/resize/image/global/product/product_1642742602095c73b6cf1564a1b5f531748436355b.png62405b38c58fe0f07fcef2367d8a9ba1/400.png",
             "title":"MSI GE76 Raider",
             "price":1420,
-            "count": 1,
+            "quantity": 1,
             "total":0
             
             
@@ -71,7 +71,7 @@ const product = [
             "image":"https://asset.msi.com/resize/image/global/product/product_1642742602095c73b6cf1564a1b5f531748436355b.png62405b38c58fe0f07fcef2367d8a9ba1/400.png",
             "title":"MSI Raider GE77 HX - 12U",
             "price":1550,
-            "count": 1,
+            "quantity": 1,
             "total":0
             
             
@@ -82,7 +82,7 @@ const product = [
             "image":"https://asset.msi.com/resize/image/global/product/product_1642743006001bf035da8f3e4e7579985458bdc810.png62405b38c58fe0f07fcef2367d8a9ba1/400.png",
             "title":"MSI Raider GE76 - 12U",
             "price":1630,
-            "count": 1,
+            "quantity": 1,
             "total":0
             
             
@@ -93,7 +93,7 @@ const product = [
             "image":"https://asset.msi.com/resize/image/global/product/product_165206346972b3177230787d73a75770296820b96a.png62405b38c58fe0f07fcef2367d8a9ba1/400.png",
             "title":"MSI Raider GE67 HX - 12U",
             "price":2010,
-            "count": 1,
+            "quantity": 1,
             "total":0
             
             
